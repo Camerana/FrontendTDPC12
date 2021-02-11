f97b080082c9b509dcf692444cf7e43f80de4b74
Alessio Filippucci <alessio.filippucci@cws.it>
2021-02-11 19:44:38 +0100

    updated

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,12 @@
-let setClockTime = () => {
+// function SetClockTime() {
+//     var clock = document.getElementById("clock");
+//     var now = new Date();
+//     var date = now.toLocaleDateString();
+//     var time = now.toLocaleTimeString();
+//     clock.innerHTML = '<span>' + date + '<br/>' + time + '</span>';
+// }
+
+var setClockTime = () => {
     var clock = document.getElementById("clock");
     var now = new Date();
     var date = now.toLocaleDateString();
@@ -8,3 +16,26 @@ let setClockTime = () => {
 
 setInterval(setClockTime, 1000);
 
+// function AddTextToContainer() {
+//     var container = document.getElementById("container");
+//     var titolo = document.createElement("h1");
+//     titolo.textContent = "Ciao, sono un titolo creato da JS!!!";
+//     titolo.style.color = "#ff0000";
+//     container.appendChild(titolo);
+//     container.style.textAlign = 'center';
+// }
+
+var addTextToContainer = () => {
+    var container = document.getElementById("container");
+    var titolo = document.createElement("h1");
+    titolo.textContent = "Ciao, sono un titolo creato da JS!!!";
+    titolo.style.color = "#ff0000";
+    container.appendChild(titolo);
+    container.style.textAlign = 'center';
+}
+
+var myButton = document.getElementById('myButton');
+myButton.addEventListener('click', addTextToContainer)
+
+var myTodoText = document.getElementById('todoText');
+myTodoText.value;
