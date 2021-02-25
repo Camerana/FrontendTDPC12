86c948996b1a569bd444ed2a140813877e7c4de5
Alessio Filippucci <alessio.filippucci@cws.it>
2021-02-25 21:34:29 +0100

    Some update

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,51 @@
+// var listaPersona = [
+//     {
+//         "nome": "Alessio",
+//         "cognome": "Filippucci",
+//     },
+//     {
+//         "nome": "Alessio",
+//         "cognome": "Filippucci",
+//     },
+//     {
+//         "nome": "Alessio",
+//         "cognome": "Filippucci",
+//     },
+//     {
+//         "nome": "Alessio",
+//         "cognome": "Filippucci",
+//     },
+//     {
+//         "nome": "Alessio",
+//         "cognome": "Filippucci",
+//     },
+//     {
+//         "nome": "Alessio",
+//         "cognome": "Filippucci",
+//     }
+// ]
+
 window.onload = Init;
 
 function Init() {
     setInterval(setClockTime, 1000);
+
+    document.getElementById("btnGetData").addEventListener("click", GetData);
+
+    // // AGGIUNTA DI NUOVO ELEMENTO NELLA COLLECTION
+    // listaPersona.push({
+    //     nome: "Paolo",
+    //     cognome: "Rossi"
+    // });
+
+    // listaPersona.forEach((persona, index) => {
+    //     if (index === 1) {
+    //         // MODIFICA DI UN ELEMENTO DELLA COLLECTION
+    //         persona.nome = "Paolino";
+    //         persona.cognome = "Paperino";
+    //     }
+    //     addPersonaToTable(persona.nome, persona.cognome);
+    // })
 }
 
 var setClockTime = () => {
@@ -12,53 +56,11 @@ var setClockTime = () => {
     clock.innerHTML = '<span>' + date + '<br/>' + time + '</span>';
 }
 
-
-var listaPersona = [
-    {
-        "nome": "Alessio",
-        "cognome": "Filippucci",
-        "eta": 32,
-        "patente": true,
-        "linguaggiDiPrograzmmazione": [
-            'C#',
-            'PHP',
-            'HTML',
-            'CSS',
-        ],
-        "lingue":[
-            {
-                "nome": "Italiano",
-                "conoscenza": "Madrelingua"
-            },
-            {
-                "nome": "Inglese",
-                "conoscenza": "...."
-            },
-            {
-                "nome": "Francese",
-                "conoscenza": "...."
-            }
-        ],
-        "creaAlert": (messaggio) => {
-            alert(messaggio)
-        }
-    }
-]
-
-var addPersonaToTable = () => {
+var addPersonaToTable = (nome, cognome) => {
     var tablePersona = document.getElementById("tabellaPersone");
-
-    var nome = "Alessio";
-    var cognome = "Filippucci";
-
     var rowPersona = document.createElement("tr");
-
     var tdNome = document.createElement("td");
     var tdCognome = document.createElement("td");
-
-    // var spanName = document.createElement("span");
-    // spanName.textContent = nome;
-    // tdNome.appendChild(spanName);
 
     tdNome.textContent = nome;
     tdCognome.textContent = cognome;
@@ -67,4 +69,42 @@ var addPersonaToTable = () => {
     rowPersona.appendChild(tdCognome);
 
     tablePersona.appendChild(rowPersona);
+}
+
+function GetData() {
+    var req = new XMLHttpRequest();
+
+    req.addEventListener('load', () => {
+        if (req.status === 200) {
+            var data = JSON.parse(req.responseText);
+            console.log(data);
+            FillData(data)
+        }
+        else{
+            alert("Errore durante il recupero dei dati. Riprovare più tardi.")
+        }
+    })
+
+    // URL PER RECUPERO DATI OPEN SOURCE => https://jsonplaceholder.typicode.com
+
+    req.open('get', "JS/data.json", true);
+    req.setRequestHeader('Content-type', 'application/json')
+    req.send();
+}
+
+function FillData(data) {
+    // AGGIUNTA DI NUOVO ELEMENTO NELLA COLLECTION
+    data.push({
+        nome: "Paolo",
+        cognome: "Rossi"
+    });
+
+    data.forEach((persona, index) => {
+        if (index === 1) {
+            // MODIFICA DI UN ELEMENTO DELLA COLLECTION
+            persona.nome = "Paolino";
+            persona.cognome = "Paperino";
+        }
+        addPersonaToTable(persona.nome, persona.cognome);
+    })
 }
