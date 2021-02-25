3f55e4010806f1c3a48bcd8cf2d68913b9944b79
Alessio Filippucci <alessio.filippucci@cws.it>
2021-02-25 18:44:25 +0100

    Uploaded

--- JS/script.js
+++ JS/script.js
@@ -1,60 +1,7 @@
-var todoListItems = [
-    'Alzarsi',
-    'Lavarsi',
-    'Fare colazione',
-    'Andare a lavoro :(',
-]
-
-
 window.onload = Init;
-//window.addEventListener("load", Init);
 
 function Init() {
     setInterval(setClockTime, 1000);
-
-    //document.addEventListener("NOME EVENTO", "CALLBACK")
-
-    //NOME_EVENTO => https://www.w3schools.com/jsref/dom_obj_event.asp
-    //CALLBACK =>   FUNZIONE CHE VIENE ESEGUITA ALL'EVENTO DEFINITO
-
-    //FUNZIONE:
-    //
-    // function Test() {
-    // }
-    //
-    // var test = () => { }
-
-    // document.getElementById("btnAddToDoList").addEventListener("click", Test);
-    // document.getElementById("btnAddToDoList").addEventListener("click", test);
-    // document.getElementById("btnAddToDoList").addEventListener("click", () => {});
-
-    // document.getElementById("btnAddToDoList").onclick = () => {
-    //     var newTextItem = document.getElementById("txtItem");
-    //     addToList(newTextItem.value);
-    //     newTextItem.value = "";
-    // };
-    document.getElementById("btnAddToDoList").addEventListener("click", () => {
-        var newTextItem = document.getElementById("txtItem");
-        addToList(newTextItem.value);
-        newTextItem.value = "";
-    })
-
-    document.getElementById("clearToDoList").addEventListener("click", () => {
-        // var allItems = document.querySelectorAll("#todoList li");
-
-        // allItems.forEach((item) => {
-        //     item.remove();
-        // })
-
-        document.getElementById("todoList").innerHTML = "";
-
-    })
-
-    todoListItems.forEach((todoListItem) => {
-        addToList(todoListItem);
-    })
-
-    addPersonaToTable();
 }
 
 var setClockTime = () => {
@@ -65,24 +12,6 @@ var setClockTime = () => {
     clock.innerHTML = '<span>' + date + '<br/>' + time + '</span>';
 }
 
-// foreach(string todoListItem in todoListItems){
-// }
-
-var addToList = (value) => {
-    var item = document.createElement("li");
-    item.textContent = value;
-    
-    var removeButton = document.createElement("button");
-    removeButton.type = "button";
-    removeButton.textContent = "Remove Item";
-    removeButton.addEventListener("click", () => item.remove());
-
-    item.appendChild(removeButton);
-
-    document.getElementById("todoList").appendChild(item);
-}
-
-// JSON (JavaScript Object Notation)
 
 var listaPersona = [
     {
@@ -115,8 +44,6 @@ var listaPersona = [
         }
     }
 ]
-
-
 
 var addPersonaToTable = () => {
     var tablePersona = document.getElementById("tabellaPersone");
